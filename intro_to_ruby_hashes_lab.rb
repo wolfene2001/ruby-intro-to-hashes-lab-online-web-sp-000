0a870bdd8ee5d38930524d54b535d4b30e13025d
wolfene2001 <wolfene2001@yahoo.com>
2019-11-13 22:49:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
 	 			:pennsylvania_railroad => {},
 	 			:b_and_o_railroad => {},
 	 			:shortline => {}
-			},	
+			},
 	 		:rent_in_dollars => {
 	 			:one_piece_owned => 25,
 	 			:two_pieces_owned => 50,
